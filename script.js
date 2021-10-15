932341c88570e7def0b0448aad0a3d4bf2884f76
Alessandra Lai <alessandralai@hotmail.com>
2021-10-15 12:16:21 +0100

    "fix background bug"

--- script.js
+++ script.js
@@ -8,61 +8,78 @@ let bmiCalc = () => {
   let height = document.querySelector("#height").value;
   let weight = document.querySelector("#weight").value;
   let bmi = Math.round((weight / height ** 2) * 10) / 10;
-  // userBMI.textContent = bmi;
-  description.style.display = "block";
+  // userBMI.textContent = bmi; // no timer
 
-  let output = 0;
-  const timer = setInterval(() => {
-    userBMI.textContent = output.toFixed(1);
-    if (output.toFixed(1) == bmi) {
-      clearInterval(timer);
-    } else {
-      output += 0.1;
+  if (height.length && weight.length) {
+    let output = 0;
+    const timer = setInterval(() => {
+      userBMI.textContent = output.toFixed(1);
+      if (output.toFixed(1) == bmi) {
+        clearInterval(timer);
+      } else {
+        output += 0.1;
+      }
+    }, 0.5);
+
+    description.style.display = "block";
+    if (bmi < 18.5) {
+      description.textContent = "Underweight";
+      blue();
+    } else if (bmi == 18.5 || bmi <= 24.9) {
+      description.textContent = "Normal weight";
+      green();
+    } else if (bmi <= 29.9) {
+      description.textContent = "Overweight";
+      yellow();
+    } else if (bmi <= 34.9) {
+      description.textContent = "Obese (class I)";
+      orange();
+    } else if (bmi <= 39.9) {
+      description.textContent = "Obese (class II)";
+      red();
+    } else if (bmi > 40) {
+      description.textContent = "Obese (class III)";
+      red();
     }
-  }, 1);
-
-  if (bmi < 18.5) {
-    description.textContent = "Underweight";
-    blue();
-  } else if (bmi == 18.5 || bmi <= 24.9) {
-    description.textContent = "Normal weight";
-    green();
-  } else if (bmi <= 29.9) {
-    description.textContent = "Overweight";
-    yellow();
-  } else if (bmi <= 34.9) {
-    description.textContent = "Obese (class I)";
-    orange();
-  } else if (bmi <= 39.9) {
-    description.textContent = "Obese (class II)";
-    red();
-  } else if (bmi > 40) {
-    description.textContent = "Obese (class III)";
-    red();
   }
 };
 
 function blue() {
+  body.className = "";
+  userBMI.className = "bmi";
+  h1.className = "";
   body.classList.add("body--blue");
   userBMI.classList.add("bmi--blue");
   h1.classList.add("h1--blue");
 }
 function green() {
+  body.className = "";
+  userBMI.className = "bmi";
+  h1.className = "";
   body.classList.add("body--green");
   userBMI.classList.add("bmi--green");
   h1.classList.add("h1--green");
 }
 function yellow() {
+  body.className = "";
+  userBMI.className = "bmi";
+  h1.className = "";
   body.classList.add("body--yellow");
   userBMI.classList.add("bmi--yellow");
   h1.classList.add("h1--yellow");
 }
 function orange() {
+  body.className = "";
+  userBMI.className = "bmi";
+  h1.className = "";
   body.classList.add("body--orange");
   userBMI.classList.add("bmi--orange");
   h1.classList.add("h1--orange");
 }
 function red() {
+  body.className = "";
+  userBMI.className = "bmi";
+  h1.className = "";
   body.classList.add("body--red");
   userBMI.classList.add("bmi--red");
   h1.classList.add("h1--red");
